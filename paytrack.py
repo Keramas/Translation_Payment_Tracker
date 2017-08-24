470631ddc4a9bd62e0b5242ce702298b43eb1573
Keramas <devincasadey@gmail.com>
2017-08-23 20:57:49 -0400

    Update paytrack.py

--- paytrack.py
+++ paytrack.py
@@ -21,18 +21,20 @@ def wordcount(value):
 value = getText(filename)
 
 #Display total word count as well as total cost for the job.
+#value currently set to 5 yen per word.
 
 print("Total number of words:", wordcount(value))
-print("Total cost:", (wordcount(value))*5,"yen")    \\value currently set to 5 yen per word.
+print("Total cost:", (wordcount(value))*5,"yen")    
 
 #Output date and payment amount to CSV
+#change FILENAME to the path and name of your CSV
 
 now = datetime.now()
 
 Date = now.strftime('%m/%d/%Y')
 Cost = (wordcount(value))*5
 
-with open(r"FILENAME.csv", 'a', newline='') as f:   \\change FILENAME to the path and name of your CSV
+with open(r"FILENAME.csv", 'a', newline='') as f:  
     w = csv.writer(f, delimiter=',')
     w.writerow([Date, Cost])
 
